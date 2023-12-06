7588d6839748553ceb352b6fd0bdf542dd26ba30
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-12-06 10:47:01 -0300

    Veiculos Maritimos Atualizado

--- 6_heranca/Navio.java
+++ 6_heranca/Navio.java
@@ -3,10 +3,22 @@ public class Navio extends VeiculoMaritimo{
     //navio
     String nome;
 
-    public Navio(String fabricante,String empresa, String modelo,int codigo,String pais, double posX, double posY,double posZ,double valor, int passageiros, double posz, double profundidadeMax, String nome){
-        super(fabricante, empresa, modelo, codigo, pais, posX, posY, valor,passageiros);
+    public Navio(String fabricante,String empresa, String modelo,int codigo,String pais, double posX, double posY,double valor, int passageiros, String nome, String dono){
+        super(fabricante, empresa, modelo, codigo, pais, posX, posY, valor,passageiros, dono);
         this.nome=nome;
         
+    }
 
+    public void Info(){
+        System.out.println("=================================="+"\n"
+                            +"Nome: "+nome+"\n"
+                            +"Fabricante: "+fabricante+"\n"
+                            +"Modelo: "+modelo+"\n"
+                            +"Empresa: "+empresa+"\n"
+                            +"País: "+pais+"\n"
+                            +"Código: "+codigo+"\n"
+                            +"Número de Passageiros: "+passageiros+"\n"
+                            +"Preço: R$"+String.format("%.2f", +valor)+"\n"
+                            +"==================================");
     }
 }
--- 6_heranca/Submarino.java
+++ 6_heranca/Submarino.java
@@ -4,14 +4,37 @@ public class Submarino extends VeiculoMaritimo{
     double posZ;
     double profundidadeMax;
 
-    public Submarino(String fabricante,String empresa, String modelo,int codigo,String pais, double posX, double posY,double posZ,double valor, int passageiros, double posz, double profundidadeMax){
-        super(fabricante, empresa, modelo, codigo, pais, posX, posY, valor,passageiros);
-        this.posZ=posz;
+    public Submarino(String fabricante,String empresa, String modelo,int codigo,String pais, double posX, double posY,double posZ,double valor, int passageiros, double profundidadeMax, String dono){
+        super(fabricante, empresa, modelo, codigo, pais, posX, posY, valor,passageiros, dono);
+        this.posZ=posZ;
         this.profundidadeMax=profundidadeMax;
     }
 
-    @Override
-    public void deslocamento(double x, double y) {
-        super.deslocamento(x,y);
+    public void deslocamento(double deslocaX, double deslocaY, double deslocaZ) {
+        
+        this.posX += deslocaX;
+        this.posY += deslocaY;
+        this.posZ += deslocaZ;
+
+        if (this.posZ >= profundidadeMax) {
+            System.out.println("Profundidade máxima foi atingida.");
+        } else {
+            System.out.println("Posicoes do Veículo após o deslocamento:"
+        +"\n"+"Posicao X: "+posX
+        +"\n"+"Posicao Y: "+posY
+        +"\n"+"Posicao Z: "+posZ);
+        }
+    }
+    public void Info(){
+        System.out.println("=================================="+"\n"
+                            +"Fabricante: "+fabricante+"\n"
+                            +"Modelo: "+modelo+"\n"
+                            +"Empresa: "+empresa+"\n"
+                            +"País: "+pais+"\n"
+                            +"Código: "+codigo+"\n"
+                            +"Número de Passageiros: "+passageiros+"\n"
+                            +"Profundidade Máxima Permitida: "+profundidadeMax+" léguas"+"\n"
+                            +"Preço: R$"+String.format("%.2f", +valor)+"\n"
+                            +"==================================");
     }
 }
