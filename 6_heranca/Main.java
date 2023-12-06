11c0a666032638ca5bd434f320ace5178c433bf5
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-12-06 10:45:27 -0300

    Main para testar outras classes, criação e funções dos veículos

--- 6_heranca/Main.java
+++ 6_heranca/Main.java
@@ -1,43 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //Deve se instanciar no minimo 1 objeto de cada classe. Todos devem estar no mesmo vetor ou lista.
         //Ao ser executado o programa escreve na tela as informações dos veiculos cadastrados.
 
+        Scanner tec = new Scanner (System.in);
+
         List<Veiculo> listaDeVeiculos = new ArrayList<>();
 
-        //Terrestres
-
-        VeiculoTerrestre cam = new Caminhao("Ford", "Ford KA 1.5 SE Flex", "EUA", 230, 500, 500.600, 2, "ELIS-7385", "Robertão da Borracharia", 30, 110);
-        listaDeVeiculos.add(cam);
+        
+        VeiculoAereo avi1 = new Aviao("Azul", "Azul corp.", "Ubuntu f28",747834923, "Escócia",60, 400, 700, 40.000, 300,"Roberto Sanchez");
+        VeiculoAereo h1 = new Helicoptero("Jens and Cunny's","J&C", "Benny blue v3",993022,"Estados Unidos",800, 100, 900,300.000,30,8,"Paul Matthew");
+        listaDeVeiculos.add(h1);
+        listaDeVeiculos.add(avi1);
+        
         VeiculoTerrestre carro = new Carro("Ford", "Ford KA 1.5 SE Flex", "EUA", 230, 500, 42.900, 5, "EYIS-UJE45", "Claudio Silva", "Gasolina", "A", 110);
+        Caminhao cam = new Caminhao("Ford", "Eletronic optimus 2000", "EUA", 230, 500, 500.600, 2, "LEI7-3885", "Robertão da Borracharia", 30, 1000);
+        VeiculoTerrestre m1 = new Moto("Honda", "Honda Civic v3", "Japão", 60, 20, 20.000, 2, "PIUJ-73KL", "Armando Ruiz", "Honda","Preto Fosco");
         listaDeVeiculos.add(carro);
-        VeiculoTerrestre moto = new Moto(null, null, null, 0, 0, 0, 0, null, null, null);
-        //listaDeVeiculos.add(moto);
+        listaDeVeiculos.add(cam);
+        listaDeVeiculos.add(m1);
+        
+        VeiculoMaritimo n1 = new Navio("Harland and Wolff","White Star Line", "Titanic v2",14041912,"Irlanda",550, 600, 1000.000, 1500,"Titanic","James Cameron");
+        VeiculoMaritimo sub2 = new Submarino("Beatles", "Beatles inc.", "Yellow Submarine v1", 45464664, "Inglaterra",100, 500, 350, 20.000, 4, 30.000,"Billy Eilish");
+        listaDeVeiculos.add(n1);
+        listaDeVeiculos.add(sub2);
 
-        //Marítimos
+        //Funções
+        
+        avi1.Info(); //antes compra
+        avi1.compra(); //compra
+        avi1.Info();//depois compra
 
-        VeiculoMaritimo sub = new Submarino(null, null, null, 0, null, 0, 0, 0, 0, 0, 0, 0);
-        //listaDeVeiculos.add(sub);
-        VeiculoMaritimo navio = new Navio(null, null, null, 0, null, 0, 0, 0, 0, 0, 0, 0, null);
-        //listaDeVeiculos.add(navio);
+        carro.Info(); //antes troca
+        carro.emplacamento(tec); //troca placa
+        carro.Info(); //depois troca
 
-        //Aéreos
+        cam.Info();
+        cam.carga(500);
+        cam.descarga(200, 300);
 
-        VeiculoAereo h1 = new Helicoptero(null, null, null, 0, null, 0, 0, 0, 0, 0, 0);
-        //listaDeVeiculos.add(h1);
-        VeiculoAereo av = new Aviao(null, null, null, 0, null, 0, 0, 0, 0, 0);
-        //listaDeVeiculos.add(av);
+        sub2.deslocamento(100, 200);
 
-        // Funções
-
-        for(int i = 0; i<listaDeVeiculos.size();i++){
-            System.out.println(listaDeVeiculos.get(i));
-        }
-
-
-
+        
     }
 }
