066367f127f4d4cc30a50fdd545f18d552fe58d0
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-12-10 00:36:52 -0300

    Adicionando função info, getters e modificando atributos de Aresta

--- TrabalhoGrafos/Aresta.java
+++ TrabalhoGrafos/Aresta.java
@@ -1,13 +1,32 @@
 public class Aresta {
-    Vertice cidade1;
-    Vertice cidade2;
-    float distancia;
+    //conexões
+    
+    private Vertice cidade1; //cidade origem
+    private Vertice cidade2; //cidade destino
+    private float distancia; //distancia entre elas
 
     public Aresta(Vertice cidade1,Vertice cidade2,float distancia){
-
+        this.cidade1=cidade1;
+        this.cidade2=cidade2;
+        this.distancia=distancia;
     }
 
     public void infoAresta(){
-        
+        System.out.println("///////////Informações das Conexões///////////"+"\n"+
+                            "Cidade 1: "+getCidade1()+"\n"+
+                            "Cidade 2: "+getCidade2()+"\n"+
+                            "Distância: "+getDistancia()+"\n");
+    }
+
+    public Vertice getCidade1() {
+        return cidade1;
+    }
+
+    public Vertice getCidade2() {
+        return cidade2;
+    }
+
+    public float getDistancia() {
+        return distancia;
     }
 }
--- TrabalhoGrafos/Grafo.java
+++ TrabalhoGrafos/Grafo.java
@@ -1,18 +1,37 @@
+import java.util.ArrayList;
+
 public class Grafo {
 
-    String [] cidades; //vertice
-    String [] conexoes; //aresta
+    private ArrayList<Vertice> cidades; //vertice
+    private ArrayList<Aresta> conexoes; //aresta
 
-    public void infoCidades(String[]cidades){
+    public void infoCidades(ArrayList<Vertice> cidades){
+        System.out.println("///////////Informações das Cidades///////////");
+        for(Vertice c : cidades){
+        System.out.println(c.getNomeCidade());
+        }
+        System.out.println("\n"+"////////////////////////////////////////////");
+    }
+    
+    public void infoConexoes(ArrayList<Aresta> conexoes){
+        System.out.println("///////////Informações das Conexões///////////"+"\n"+
+        "Cidade 1: "+getCidade1()+"\n"+
+        "Cidade 2: "+getCidade2()+"\n"+
+        "Distância: "+getDistancia()+"\n");
 
     }
-    public void infoConexoes(String conexoes){
+    public void cadastraCidade(ArrayList<Vertice> cidades){
+        System.out.println("///////////Informações das Conexões///////////"+"\n"+
+        "Cidade 1: "+getCidade1()+"\n"+
+        "Cidade 2: "+getCidade2()+"\n"+
+        "Distância: "+getDistancia()+"\n");
 
     }
-    public void cadastraCidade(String[]cidades){
-
-    }
-    public void cadastraConexao(String conexoes){
+    public void cadastraConexao(ArrayList<Aresta> conexoes){
+        System.out.println("///////////Informações das Conexões///////////"+"\n"+
+        "Cidade 1: "+getCidade1()+"\n"+
+        "Cidade 2: "+getCidade2()+"\n"+
+        "Distância: "+getDistancia()+"\n");
 
     }
     
