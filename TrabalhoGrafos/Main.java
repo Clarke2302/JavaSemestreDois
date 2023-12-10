9c153e0bdd23b37aa663a13c8e13a6a120a1a4db
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-12-10 01:48:18 -0300

    Create Main.java

---
+++ TrabalhoGrafos/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Grafo grafo = new Grafo();
+        Vertice ver = new Vertice(null);
+        Scanner scanner = new Scanner(System.in);
+
+        int opcao;
+        do {
+            System.out.println("\n1. Cadastrar cidade");
+            System.out.println("2. Cadastrar conexão");
+            System.out.println("3. Listar cidades");
+            System.out.println("4. Listar conexões");
+            System.out.println("5. Listar cidades vizinhas");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o nome da cidade: ");
+                    String nomeCidade = scanner.nextLine();
+                    grafo.cadastraCidade(scanner);
+                    break;
+                case 2:
+                    System.out.print("Digite a cidade de origem: ");
+                    String cidadeOrigem = scanner.nextLine();
+                    System.out.print("Digite a cidade de destino: ");
+                    String cidadeDestino = scanner.nextLine();
+                    System.out.print("Digite a distância entre as cidades: ");
+                    int distancia = scanner.nextInt();
+                    scanner.nextLine();
+                    grafo.cadastraConexao(scanner, distancia);
+                    break;
+                case 3:
+                    grafo.infoCidades();
+                    break;
+                case 4:
+                    grafo.infoConexoes();
+                    break;
+                case 5:
+                    System.out.print("Digite o nome da cidade: ");
+                    String cidade = scanner.nextLine();
+                    ver.infoVizinhas(cidade);
+                    break;
+                case 0:
+                    System.out.println("Encerrando o programa.");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while (opcao != 0);
+
+        scanner.close();
+    }
+}
