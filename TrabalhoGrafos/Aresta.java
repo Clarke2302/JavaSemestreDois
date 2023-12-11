f881e91e82abab2b82d95cffd00ae20fdc9aa3cf
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-12-11 13:37:09 -0300

    Adicionando infoAresta

--- TrabalhoGrafos/Aresta.java
+++ TrabalhoGrafos/Aresta.java
@@ -11,11 +11,17 @@ public class Aresta {
         this.distancia=distancia;
     }
 
-    public void infoAresta(){
-        System.out.println("\n"+"///////////Informações das Conexões///////////"+"\n"+
-                            "Cidade 1: "+getCidade1()+"\n"+
-                            "Cidade 2: "+getCidade2()+"\n"+
-                            "Distância: "+getDistancia()+"\n");
+    public Aresta(){
+        
+    }
+
+    public void infoAresta(Aresta ab){
+        System.out.println("\n"+"///////////Informações da Aresta///////////");
+        
+        System.out.println("Cidade 1: "+ab.getCidade1().getNomeCidade()+"\n"
+        +"Cidade 2: "+ab.getCidade2().getNomeCidade()+"\n"+"Distância entre: "+ab.getDistancia()+" km de distância");
+        
+        System.out.println("\n"+"////////////////////////////////////////////");
     }
 
     public Vertice getCidade1() {
