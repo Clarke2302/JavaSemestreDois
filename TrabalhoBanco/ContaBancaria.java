1f6d2561dc18c9dbe3fa6d4829e7eacfdbef44e3
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-12-13 23:15:05 -0300

    Update ContaBancaria.java

--- TrabalhoBanco/ContaBancaria.java
+++ TrabalhoBanco/ContaBancaria.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public abstract class ContaBancaria {
@@ -20,86 +21,130 @@ public abstract class ContaBancaria {
         
     }
  //------------Sacar--------------------
-    public void saque(Scanner tec){
+    public void saque(Scanner tec,ArrayList<ContaBancaria> contas){
         System.out.println("\n"+":Menu Saque:");
 
-        if(verificaSenha(tec)==true){
-        
-        System.out.println("Qual o valor a ser sacado: ");
-        double sacar = tec.nextDouble();
+        for(ContaBancaria cb : contas){
 
-            if(sacar>saldo){
-                System.out.println("\n"+"Seu saldo é insuficiente para essa retirada!"+"\n"+"Digite um valor menor!");
-            }else if(saldo>sacar){
-                saldo -= sacar;
-                System.out.println("\n"+"Ok, tudo certo com o saque!"+"\n"+"Você sacou R$"+sacar+" seu saldo agora é de R$ "+saldo);
+            System.out.println("Digite o número da conta: ");
+            int num = tec.nextInt();
+
+            if(cb.getNumConta() == num){
+
+                if(verificaSenha(tec, contas)==true){
+                
+                System.out.println("Qual o valor a ser sacado: ");
+                double sacar = tec.nextDouble();
+
+                    if(sacar>cb.getSaldo()){
+                        System.out.println("\n"+"Seu saldo é insuficiente para essa retirada!"+"\n"+"Digite um valor menor!");
+                    }else if(cb.getSaldo()>sacar){
+                        double novoSaldo = sacar - saldo;
+                        cb.setSaldo(novoSaldo);
+                        System.out.println("\n"+"Ok, tudo certo com o saque!"+"\n"+"Você sacou R$"+sacar+" seu saldo agora é de R$ "+saldo);
+                    }
+                }
             }
         }
     }
 
-    public void saque(double sacar, Scanner tec){
+    public void saque(double sacar, Scanner tec, ArrayList<ContaBancaria> contas, int num){
         System.out.println("\n"+":Menu Saque:");
 
-        System.out.println("Primeiramente precisamos que digite a sua senha: "+"\n");
-        int senhhh = tec.nextInt();
+        for(ContaBancaria cb : contas){
+            
+            if(cb.getNumConta() == num){
+        
+            System.out.println("Agora precisamos que digite a sua senha: "+"\n");
+            int senhhh = tec.nextInt();
+            
+                if(verificaSenha(senhhh,contas)==true){
 
-        if(verificaSenha(senhhh)==true){
-
-            if(sacar>saldo){
-                System.out.println("\n"+"Seu saldo é insuficiente para essa retirada!"+"\n"+"Digite um valor menor!");
-            }else if(saldo>sacar){
-                saldo -= sacar;
-                System.out.println("\n"+"Ok, tudo certo com o saque!"+"\n"+"Você sacou R$"+sacar+" seu saldo agora é de R$ "+saldo);
+                    if(sacar>cb.getSaldo()){
+                        System.out.println("\n"+"Seu saldo é insuficiente para essa retirada!"+"\n"+"Digite um valor menor!");
+                    }else if(cb.getSaldo()>sacar){
+                        double novoSaldo = sacar - saldo;
+                        cb.setSaldo(novoSaldo);
+                        System.out.println("\n"+"Ok, tudo certo com o saque!"+"\n"+"Você sacou R$"+sacar+" seu saldo agora é de R$ "+saldo);
+                    }
+                }
             }
         }
     }
     //-------------Depositar---------------
-    public void deposito(Scanner tec){
-        System.out.println("\n"+":Menu Depósito:"+"\n"+
-        "Qual o valor a ser depositado: ");
-        double depositar = tec.nextDouble();
+    public void deposito(Scanner tec,ArrayList<ContaBancaria> contas){
+        System.out.println("\n"+":Menu Depósito:");
+        
+        for(ContaBancaria cb : contas){
 
-        if(depositar<10){
-            System.out.println("\n"+"Você não pode depositar um valor abaixo de R$ 10,00"+"\n"+"Digite um valor mínimo de R$ 10,00!");
-        }else if(depositar>=10){
-            saldo += depositar;
-            System.out.println("\n"+"Você depositou R$ "+depositar+" seu saldo agora é de R$ "+saldo);
+            System.out.println("Digite o número da conta: ");
+            int num = tec.nextInt();
+            
+            if(cb.getNumConta() == num){
+
+                System.out.println("Qual o valor a ser depositado: ");
+                double depositar = tec.nextDouble();
+
+                if(depositar<10){
+                    System.out.println("\n"+"Você não pode depositar um valor abaixo de R$ 10,00"+"\n"+"Digite um valor mínimo de R$ 10,00!");
+                }else if(depositar>=10){
+                    
+                    double novoSaldo =saldo + depositar;
+                    cb.setSaldo(novoSaldo);
+                    System.out.println("\n"+"Você depositou R$ "+depositar+" seu saldo agora é de R$ "+saldo);
+                }
+            }
         }
     }
 
-    public void deposito(double depositar){
+    public void deposito(double depositar,ArrayList<ContaBancaria> contas, int num){
         System.out.println("\n"+":Menu Depósito:"+"\n");
 
-        if(depositar<10){
-            System.out.println("\n"+"Você não pode depositar um valor abaixo de R$ 10,00"+"\n"+"Digite um valor mínimo de R$ 10,00!");
-        }else if(depositar>=10){
-            saldo += depositar;
-            System.out.println("\n"+"Você depositou R$ "+depositar+" seu saldo agora é de R$ "+saldo);
+        for(ContaBancaria cb : contas){
+            
+            if(cb.getNumConta() == num){
+
+                if(depositar<10){
+                    System.out.println("\n"+"Você não pode depositar um valor abaixo de R$ 10,00"+"\n"+"Digite um valor mínimo de R$ 10,00!");
+                }else if(depositar>=10){
+                    double novoSaldo =saldo + depositar;
+                    cb.setSaldo(novoSaldo);
+                    System.out.println("\n"+"Você depositou R$ "+depositar+" seu saldo agora é de R$ "+saldo);
+                }
+            }
         }
     }
     //----------------Verificar Senha-------------------
-    public boolean verificaSenha(Scanner tec){
-        System.out.println("\n"+"Digite a sua senha: ");
-        String suaSenha = tec.next();
-        
-        if(senha.equalsIgnoreCase(suaSenha)){
-            return true;
-        }else{
-            System.out.println("A senha está incorreta!");
-            return false;
+    public boolean verificaSenha(Scanner tec,ArrayList<ContaBancaria> contas){
+        for(ContaBancaria cb : contas){
+
+            System.out.println("\n"+"Digite a sua senha: ");
+            String suaSenha = tec.next();
+            
+            if(cb.getSenha().equalsIgnoreCase(suaSenha)){
+                return true;
+            }else{
+                System.out.println("A senha está incorreta!");
+                return false;
+            }
         }
+        return false;
         
     }
 
-    public boolean verificaSenha(int senhaa){
+    public boolean verificaSenha(int senhaa,ArrayList<ContaBancaria> contas){
         String senhaEmString = String.valueOf(senhaa);
 
-        if(senha.equalsIgnoreCase(senhaEmString)){
-            return true;
-        }else{
-            System.out.println("A senha está incorreta!");
-            return false;
+        for(ContaBancaria cb : contas){
+
+            if(cb.getSenha().equalsIgnoreCase(senhaEmString)){
+                return true;
+            }else{
+                System.out.println("A senha está incorreta!");
+                return false;
+            }
         }
+        return true;
     }
 
     //Getters and Setters
