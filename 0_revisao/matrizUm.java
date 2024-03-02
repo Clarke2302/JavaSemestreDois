d9a05e1ab84fe6010fb88044e517d6a0b5419073
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2024-03-02 16:04:34 -0300

    Create matrizUm.java

---
+++ 0_revisao/matrizUm.java
@@ -0,0 +1,23 @@
+package atividadesRevisao;
+import java.util.Scanner;
+public class matrizUm {
+public static void main(String[]args){
+Scanner tec = new Scanner(System.in);
+
+//dois colchetes para trabalharmos com matrizes
+//colunas e linhas
+//ou seja 5 colunas e 4 linhas porque começamos no 0
+String [][] nomes = new String [6][4];
+
+//i==coluna  j==linhas
+
+for(int i=0; i<6;i++){
+    for(int j=0; j<4;j++){
+        
+    }
+}
+
+
+
+    }
+}
